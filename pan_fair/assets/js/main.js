452b3c838c3a71da257d26e8a6c22b938800f292
yoshie <amelie.p.design@gmail.com>
2024-12-01 15:39:49 +0900

    1201,1539

---
+++ pan_fair/assets/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+{
+    $(function(){
+        $('.header__btn').on('click', function(){
+            $('.nav').toggleClass('active');
+        });
+
+        $('.nav__btn, .nav__item a').on('click', function(){
+            $('.nav').removeClass('active');
+        });
+    });
+
+}
