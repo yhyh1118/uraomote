6a2cee2c6eac28769625b55fc2960b071917994a
hayamayoshie <hayama.yoshie@isid.co.jp>
2024-12-05 16:42:03 +0900

    1205 1641 light -dark適用できそう

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,8 @@
             }, 400);
         });
 
+
+
         //dark-light
         $('.moon').on('click', function () {
             $('.dark-light').addClass('switch');
@@ -22,8 +24,15 @@
 
         $('.sun').on('click', function () {
             $('.dark-light').removeClass('switch');
-
         });
+
+
+        ///////////////////////////////////////////////////////////////////////
+
+
+
+        // ///////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
         // banner
         var pos = 0; // 前回のスクロール位置
@@ -45,7 +54,6 @@
                     $('body').css('overflow', 'auto'); // スクロールを再度有効化
                 }, 1100); // 3000ミリ秒 (3秒) 後に実行
             }
-
             pos = scrollTop; // 現在の位置を保存
         }
 
@@ -57,34 +65,82 @@
             banner.addClass('hide'); // バナーを非表示にする
             $('main').css('overflow', 'auto'); // スクロールを有効化
         });
-    });
 
+        // お試し/
 
-    // チェックボックスの取得
-    const btn = document.querySelector("#btn-mode");
+        // 要素やクラスを指定しておく
+        const checkToggle = document.getElementById('js_mode_toggle');
+        const rotateIcon = document.getElementById('js_rotate');
+        const classLight = 'js-mode-light';
 
-    // チェックした時の挙動
-    btn.addEventListener("change", () => {
-        if (btn.checked == true) {
-            // ダークモード
-            document.body.classList.remove("light-theme");
-            document.body.classList.add("dark-theme");
-        } else {
-            // ライトモード
-            document.body.classList.remove("dark-theme");
-            document.body.classList.add("light-theme");
+        // デバイスがライトモードかどうかチェック
+        const isLight = window.matchMedia('(prefers-color-scheme: light)').matches;
+
+        // ローカルストレージに保存するための適当なKey名
+        const keyLocalStorage = 'whike-theme-mode';
+
+        // ローカルストレージの情報を取得
+        const localTheme = localStorage.getItem(keyLocalStorage);
+
+        // 絵文字を回転させる角度
+        let nowRotate = 0;
+
+        // ローカルストレージの中身と、端末がライトモードかどうか（ie,edgeには無意味）をチェック
+        if (localTheme === 'light') {
+            // ローカルストレージの情報が優先
+            rotateInfinite();
+            changeMode('light');
+        } else if (localTheme === 'dark') {
+            changeMode('dark');
+        } else if (isLight) {
+            rotateInfinite();
+            changeMode('light');
         }
+
+        // チェックボックスでの切り替え、選択をローカルストレージに保存
+        // モード切替スイッチが変更されたら発動
+        checkToggle.addEventListener('change', function (e) {
+            // 絵文字大回転
+            rotateInfinite();
+
+            // チェックされたらライトモード、されなければダークモードにし、ローカルストレージにどちらを選んだか保存する
+            if (e.target.checked) {
+                changeMode('light');
+                localStorage.setItem(keyLocalStorage, 'light');
+            } else {
+                changeMode('dark');
+                localStorage.setItem(keyLocalStorage, 'dark');
+            }
+        });
+
+        /**
+         * テーマ切り替え
+         * @param {String} mode 'light' もしくは 'dark'
+         */
+        function changeMode(mode) {
+            // 引数にしたがってbodyにクラスをつける
+            // チェックボックス経由で変更かかったときはいいんだけど、ローカルストレージとかからモードを変えた場合にチェック状態がおかしくなるので、合わせておく
+            if (mode === 'light') {
+                document.body.classList.add(classLight);
+                checkToggle.checked = true;
+            } else if (mode === 'dark') {
+                document.body.classList.remove(classLight);
+                checkToggle.checked = false;
+            }
+        }
+
+        /**
+         * 月と太陽アイコン無限回転
+         * 呼ばれるたびに180度角度が追加されていく
+         */
+        function rotateInfinite() {
+            nowRotate += 180;
+            rotateIcon.style.transform = 'rotate(' + nowRotate + 'deg)';
+        }
+
+
+        // 
+
+
     });
-
-    // dark-light要素を取得
-    const toggleSwitch = document.getElementById('toggleSwitch');
-
-    // クリックイベントを追加
-    toggleSwitch.addEventListener('click', () => {
-        // switchクラスをトグル
-        toggleSwitch.classList.toggle('switch');
-    });
-
-
-
 }
