d6af1461054fbe3dfcbe0037bd3682a99b40c610
yoshie <amelie.p.design@gmail.com>
2025-01-16 22:26:33 +0900

    01162226

--- assets/js/top.js
+++ assets/js/top.js
@@ -69,27 +69,6 @@
             horizontal: false
         });
 
-        let throttle = (func, limit) => {
-            let inThrottle;
-            return function () {
-                const args = arguments;
-                const context = this;
-                if (!inThrottle) {
-                    func.apply(context, args);
-                    inThrottle = true;
-                    setTimeout(() => (inThrottle = false), limit);
-                }
-            };
-        };
-
-        // Rellaxの更新をスロットリング
-        const optimizedRellaxUpdate = throttle(() => {
-            rellax.refresh();
-        }, 100); // 100ms間隔で更新
-
-        window.addEventListener('scroll', optimizedRellaxUpdate);
-
-
     });
 }
 
