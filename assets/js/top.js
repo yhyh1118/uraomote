300157c460a9c1c5893cb08c6370f091318555a4
yoshie <amelie.p.design@gmail.com>
2025-01-22 21:42:07 +0900

    01222142

--- assets/js/top.js
+++ assets/js/top.js
@@ -13,23 +13,7 @@
         const rotateIcon = document.getElementById('js_rotate');
         const classLight = 'js-mode-light';
         const theme = 'uraomote-theme-mode';
-        // const keyTimestamp = 'uraomote-theme-timestamp';
         let nowRotate = 0;
-
-        // // 現在のタイムスタンプ
-        // const currentTime = Date.now();
-        // // 1時間の有効期限（ミリ秒）
-        // const expiryTime = 3600 * 1000;
-
-        // // localStorageから値を取得し、期限切れを確認
-        // const storedMode = localStorage.getItem(theme);
-        // const storedTimestamp = localStorage.getItem(keyTimestamp);
-
-        // // 有効期限が切れている場合、localStorageをクリア
-        // if (storedTimestamp && currentTime - parseInt(storedTimestamp, 10) > expiryTime) {
-        //     localStorage.removeItem(theme);
-        //     localStorage.removeItem(keyTimestamp);
-        // }
 
         // テーマモードの初期化
         if (localStorage.getItem(theme) === 'light') {
@@ -64,54 +48,35 @@
         // rellax
         var rellax = new Rellax('.rellax', {
             center: true,
-            round: false,   // 小数点以下の計算を減らすことで負荷軽減
-            vertical: true, // 水平方向の動きが不要なら無効化
+            round: false,
+            vertical: true, 
             horizontal: false
         });
 
-        const mySplide = new Splide( '.splide' );
-        mySplide.mount();
+        const options = {
+            type: "loop", // ループさせる
+            arrows: false, // 矢印ボタンを非表示
+            pagination: false, // ページネーションを非表示
+            drag: "free", // フリードラッグモード
+            gap: 72, // スライド間の余白
+            perPage: 4, // 表示するスライドの枚数
+            breakpoints: {
+              769: {
+                perPage: 1.5, // 画面幅500px未満で表示枚数1枚
+                gap: 48, // 画面幅500px未満でスライド間の余白0
+              },
+              1440: {
+                perPage: 3, // 画面幅500px未満で表示枚数1枚
+                gap: 48, // 画面幅500px未満でスライド間の余白0
+              },
+            },
+            autoScroll: {
+              speed: 0.5, // スクロール速度
+              pauseOnHover: false, // カーソルが乗ってもスクロールを停止させない
+            },
+          };
+          const splide = new Splide(".splide", options);
+          splide.mount(window.splide.Extensions);
 
     });
 }
-
-
-// banner_bk
-
-        // // バナー表示・非表示の切り替え処理
-        // var pos = 0; // 前回のスクロール位置
-        // var banner = $('.banner');
-        // var isForcedScroll = false;
-
-        // $(window).on('scroll', function () {
-        //     var scrollTop = $(window).scrollTop();
-
-        //     //強制スクロール中は何もしない
-        //     if (isForcedScroll) return;
-
-        //     if (banner.hasClass('hide') && scrollTop === 0 && scrollTop < pos) {
-        //         banner.removeClass('hide'); // 上にスクロールしてページの上部に来たらバナーを表示
-        //     } else if (!banner.hasClass('hide') && scrollTop > pos) {
-        //         banner.addClass('hide'); // 下にスクロールでバナーを非表示
-        //         $('body').css('overflow', 'hidden');
-
-        //         //0.1s後にトップにスクロール
-        //         setTimeout(function () {
-        //             isForcedScroll = true;
-        //             window.scrollTo(0, 0);
-
-        //             //1.5後に、スクロール有効か
-        //             setTimeout(function () {
-        //                 isForcedScroll = false;
-        //                 $('body').css('overflow', 'auto');
-        //             }, 1500);
-        //         }, 100);
-        //     }
-
-        //     pos = scrollTop; // 現在の位置を保存
-        // });
-
-        // banner.on('click', function () {
-        //     banner.addClass('hide'); // バナーを非表示にする
-        //     $('main').css('overflow', 'auto'); // スクロールを再度有効化
-        // });
